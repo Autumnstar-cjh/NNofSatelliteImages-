af061678b4476a5ad9fa303f1a045352164f4d3d
Autumnstar <69102692+Autumnstar-cjh@users.noreply.github.com>
2024-03-05 23:45:30 -0500

    Update getStreetView.py
    Signed-off-by: Autumnstar <69102692+Autumnstar-cjh@users.noreply.github.com>

--- getStreetView.py
+++ getStreetView.py
@@ -4,7 +4,7 @@ import urllib.request
 import os
 
 data = pd.read_excel('FL_Dataset/All_Coordinates.xlsx')
-gmaps = googlemaps.Client(key='AIzaSyCg9VPSQJXtKuCJ1viKfZuzIrUXqgDzGRg')
+gmaps = googlemaps.Client(key='*')
 i = 0
 
 # 图片保存的文件夹路径
@@ -20,7 +20,7 @@ for index, row in data.iterrows():
     pitch = 0  # 你可以根据实际情况调整此值
     radius = 3000
     source = 'outdoor'
-    url = f"https://maps.googleapis.com/maps/api/streetview?location={latitude},{longitude}&size={size}&fov={fov}&heading={heading}&pitch={pitch}&radius={radius}&source={source}&key=AIzaSyCg9VPSQJXtKuCJ1viKfZuzIrUXqgDzGRg"
+    url = f"https://maps.googleapis.com/maps/api/streetview?location={latitude},{longitude}&size={size}&fov={fov}&heading={heading}&pitch={pitch}&radius={radius}&source={source}&key=*"
     # 文件保存路径
     file_path = os.path.join(folder_path, f"{i}.jpg")
     # 下载图片并保存
